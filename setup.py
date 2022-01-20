7bfc7bebd4f8040b37fd5e3bae0904ef1eb833f4
Gareth Jones <oxidising@gmail.com>
2022-01-20 10:27:02 +0000

    Fix for missing packages when importing cfchecker before install

--- setup.py
+++ setup.py
@@ -1,44 +1,43 @@
 from setuptools import setup, find_packages
-import sys
 import os
+import re
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + '/src')
-__version__ = __import__('cfchecker').__version__
+with open("src/cfchecker/__init__.py", "r") as f:
+    version_line = f.readline()
+    clean_str = re.sub(r"\s+", "", version_line, flags=re.UNICODE)
+    __version__ = clean_str.split("=")[1].replace("'", "")
 
 # Need to try/except this because pip install unpacks to a different dir and
 # relative path lookup fails. Should still work where needed.
 try:
-    __description__ = "\n" + open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+    __description__ = "\n" + open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 except:
     __description__ = ""
 
-setup(name='cfchecker',
-      version=__version__,
-      description="The NetCDF Climate Forecast Conventions compliance checker",
-      long_description=__description__,
-
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: Scientific/Engineering :: Atmospheric Science',
-          'Intended Audience :: Science/Research',
-          'Natural Language :: English',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: MacOS',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python',
-      ],
-
-      keywords='',
-      author='Rosalyn Hatcher',
-      author_email='r.s.hatcher@reading.ac.uk',
-
-      url='http://cfconventions.org/compliance-checker.html',
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['netCDF4', 'numpy>=1.7', 'cfunits>=3.0.0', 'future'],
-      entry_points={'console_scripts': ['cfchecks = cfchecker.cfchecks:main']},
-      )
+setup(
+    name="cfchecker",
+    version=__version__,
+    description="The NetCDF Climate Forecast Conventions compliance checker",
+    long_description=__description__,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+    keywords="",
+    author="Rosalyn Hatcher",
+    author_email="r.s.hatcher@reading.ac.uk",
+    url="http://cfconventions.org/compliance-checker.html",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["netCDF4", "numpy>=1.7", "cfunits>=3.0.0", "future"],
+    entry_points={"console_scripts": ["cfchecks = cfchecker.cfchecks:main"]},
+)
